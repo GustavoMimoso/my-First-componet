4bf0da6ce09d235b7c252d6b2377108d412e1365
Gustavo Mimoso <guraincrivel@hotmail.com>
2025-09-17 11:37:34 -0300

    teste de de componentes default e nomeado

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+
+export function Profile () {
+    return(
+        <div>
+        <img 
+            src="https://media.licdn.com/dms/image/v2/D4D03AQF-f8Ur7SzZpA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1731351544754?e=1761177600&v=beta&t=cTt-3OaGuByc8ht-Jnb_VP2pIhQrNsDxvwqdp-qLcDw " 
+            alt="Gustavo Mimoso" 
+        />
+        <h3>Gustavo Mimoso</h3>
+        <p>Suporte TI</p>
+    </div>
+    )
+}
+
+
+export function Job () {
+    return(
+        <p>Empresa Atual: Guariglia Leilões</p>
+    )
+}
