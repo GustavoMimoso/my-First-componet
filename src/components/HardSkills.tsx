58927db70cc8a9aa39a8c4266fcc70699f73f3ea
Gustavo Mimoso <guraincrivel@hotmail.com>
2025-09-18 17:42:26 -0300

    Exemplo de Componentes aninhados

---
+++ src/components/HardSkills.tsx
@@ -0,0 +1,3 @@
+export function HardSkills (){
+    return<p>HTML CSS, JavaScript, TypeScript, PHP, Nodejs, SQL</p>;
+}
---
+++ src/components/SoftSkills.tsx
@@ -0,0 +1,4 @@
+export function SoftSkills () {
+    return <p>Comunicação, Trabalho em equipe, Organizado, Proativo, Inteligencia emocional</p>
+
+}
